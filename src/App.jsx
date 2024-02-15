70e54dbc1ba6f40dc760ff6f97b1f076c4dbd8a2
Thales <dan.console.log@gmail.com>
2024-02-15 12:20:56 +0100

    add chakra initial changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button, Flex } from "@chakra-ui/react"
+import "./App.css"
+
+function App() {
+  return (
+    <Flex>
+      <Button>Empieza por aquí!</Button>
+    </Flex>
+  )
+}
+
+export default App
